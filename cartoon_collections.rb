507806e4dcaac9479e9cb1f6a2149915b7e6fcfd
Irfan Maknojia <irfan.mak7@gmail.com>
2018-11-13 04:23:50 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,4 @@
 def roll_call_dwarves(array)
-  # Your code here
   counter = 1
   array.each do |name|
     puts "#{counter}. #{name}"
@@ -7,8 +6,12 @@ def roll_call_dwarves(array)
   end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+  new_array = []
+  array.collect do |call|
+    new_array.push("#{call.capitalize}!")
+  end
+  new_array
 end
 
 def long_planeteer_calls# code an argument here
